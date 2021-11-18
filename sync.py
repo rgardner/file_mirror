89fcff90f9d07e0be15f2b222c8157e96e8c6446
Bob Gardner <bob.hn.gardner@gmail.com>
2021-11-17 21:34:37 -0500

    Various fixes
    *   Add --config option to configure config path.
    *   Fix last_sync_time output when null
    *   Handle EOFError and KeyboardInterrupt as cancel operations
    *   Expand ~ in src and dst paths
    *   Pass src/ to avoid creating extra directory in dst
    *   Move default config location to ~/.config/file_mirror/config.json

--- sync.py
+++ sync.py
@@ -1,30 +1,55 @@
 #!/usr/bin/env python
 
+import argparse
 import datetime
 import json
 import pathlib
 import subprocess
 import sys
+from typing import List
 
 
-CONFIG_FILE = pathlib.Path.home() / "Desktop" / "sync_config.json"
+CONFIG_FILE = "~/.config/file_mirror/config.json"
 
 
-def main() -> None:
+def parse_args(args: List[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        default=CONFIG_FILE,
+        help=f"config file location, defaults to {CONFIG_FILE}",
+    )
+    return parser.parse_args(args)
+
+
+def main(raw_args: List[str]) -> None:
+    args = parse_args(raw_args)
+    print(f"Reading config from {args.config}")
+
     # Read the config file
+    config_path = pathlib.Path(args.config).expanduser()
     try:
-        with open(CONFIG_FILE, encoding="utf-8") as f:
+        with open(config_path, encoding="utf-8") as f:
             config = json.load(f)
     except FileNotFoundError:
-        sys.exit(f"Could not find {CONFIG_FILE}")
+        sys.exit(f"Could not find {config_path}")
 
     # Print the last sync time for each directory
-    for copy_spec in config["copy_specs"]:
-        print("\t".join(copy_spec[key] for key in ["src", "dst", "last_sync_time"]))
+    print("src\t\t\tdst\t\t\tlast sync time")
+    for spec in config["copy_specs"]:
+        src = spec["src"]
+        dst = spec["dst"]
+        last_sync_time = spec.get("last_sync_time") or "never"
+        print("\t".join((src, dst, last_sync_time)))
 
-    # Prompt to sync
+    # Confirm sync
     while True:
-        response = input("Sync all? [y/n] ")
+        try:
+            response = input("Sync all? [y/n] ")
+        except (EOFError, KeyboardInterrupt):
+            print("\nCanceling", file=sys.stderr)
+            sys.exit(0)
+
         if response.lower() in ("y", "yes"):
             break
 
@@ -37,26 +62,36 @@ def main() -> None:
                 file=sys.stderr,
             )
 
-    # If yes, run `rsync <src> <dst> --archive --delete --verbose`
+    # If yes, run `rsync <src>/ <dst> --archive --delete --verbose`
     for copy_spec in config["copy_specs"]:
-        src = copy_spec["src"]
-        dst = copy_spec["dst"]
+        src = pathlib.Path(copy_spec["src"]).expanduser()
+        dst = pathlib.Path(copy_spec["dst"]).expanduser()
         print(f"Syncing {src} to {dst}")
+        # Create the destination parent directory if it doesn't exist (rsync
+        # doesn't do this automatically)
+        dst.parent.mkdir(parents=True, exist_ok=True)
         try:
+            # Append '/' to avoid creating an extra directory at the
+            # destination.
+            src_arg = f"{str(src)}/"
+            # Use --archive to preserve permissions and timestamps
+            # Use --delete to delete files that are no longer in the source
             subprocess.run(
-                ["rsync", src, dst, "--archive", "--delete", "--verbose"], check=True
+                ["rsync", src_arg, dst, "--archive", "--delete", "--verbose"],
+                check=True,
             )
         except subprocess.CalledProcessError as exc:
-            print(f"rynsc failed: {exc}", file=sys.stderr)
+            print(f"rsync failed: {exc}", file=sys.stderr)
             sys.exit(1)
 
-    # Update the last sync time for each directory
+    # Update the last sync time for each directory. Just use the same time
+    # for each directory
     now = datetime.datetime.now()
-    for i, copy_spec in enumerate(config["copy_specs"]):
-        config["copy_specs"][i]["last_sync_time"] = str(now)
-    with open(CONFIG_FILE, "w", encoding="utf-8") as fp:
+    for i in range(len(config["copy_specs"])):
+        config["copy_specs"][i]["last_sync_time"] = now.isoformat()
+    with open(config_path, "w", encoding="utf-8") as fp:
         json.dump(config, fp, indent=4)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
